0d562dce8c62741a19a152abea275b13a7f76643
Ali <119946008+Azmov7H@users.noreply.github.com>
2025-03-04 02:00:44 +0200

    تحديث المشروع وإصلاح مشاكل MongoDB

--- config/db.js
+++ config/db.js
@@ -6,9 +6,7 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(colors.cyan(`MongoDB Connected: ${conn.connection.host}`));
   } catch (error) {
     console.error(colors.red(`Error: ${error.message}`));
